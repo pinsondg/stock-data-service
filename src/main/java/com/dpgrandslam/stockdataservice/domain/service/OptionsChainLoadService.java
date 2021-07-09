9b2cd6d2911b740978b4cf50ebe53d599f39d60c
Daniel <pinsondg@dukes.jmu.edu>
2021-07-09 16:32:32 -0400

    Initial performance enhancements

--- src/main/java/com/dpgrandslam/stockdataservice/domain/service/OptionsChainLoadService.java
+++ src/main/java/com/dpgrandslam/stockdataservice/domain/service/OptionsChainLoadService.java
@@ -8,6 +8,7 @@ import com.dpgrandslam.stockdataservice.domain.util.TimeUtils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.*;
