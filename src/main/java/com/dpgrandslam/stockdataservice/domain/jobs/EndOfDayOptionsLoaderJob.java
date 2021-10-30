9e01dfbae5561d13623cd8d3cc85415a0cae6ad1
Daniel <pinsondg@dukes.jmu.edu>
2021-10-30 12:49:33 -0400

    Decreasing job steps

--- src/main/java/com/dpgrandslam/stockdataservice/domain/jobs/EndOfDayOptionsLoaderJob.java
+++ src/main/java/com/dpgrandslam/stockdataservice/domain/jobs/EndOfDayOptionsLoaderJob.java
@@ -32,7 +32,7 @@ import java.util.stream.Collectors;
 @Slf4j
 public class EndOfDayOptionsLoaderJob {
 
-    private static final int STEPS = 4;
+    private static final int STEPS = 3;
 
     private static final int MAIN_JOB = 0;
     private static final int RETRY_JOB = 1;
