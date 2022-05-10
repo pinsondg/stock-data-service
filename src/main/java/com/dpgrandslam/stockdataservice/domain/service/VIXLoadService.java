39560b6dd89a0e2fad27e38073009a46b28b5074
Daniel Pinson <pinsondg@dukes.jmu.edu>
2022-05-09 19:23:52 -0700

    Optimizing vix load to get around yahoo finance errors

--- src/main/java/com/dpgrandslam/stockdataservice/domain/service/VIXLoadService.java
+++ src/main/java/com/dpgrandslam/stockdataservice/domain/service/VIXLoadService.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,10 +24,23 @@ public class VIXLoadService {
 
     public List<YahooFinanceQuote> loadVIXBetweenDates(LocalDate startDate, LocalDate endDate) {
         log.info("Loading VIX data for dates {} - {}", startDate, endDate);
-        return vixCache.get(Pair.of(startDate, endDate), (pair) -> historicStockDataLoadService
-                .loadQuoteForDates("^VIX", pair.getLeft(), pair.getRight()).stream()
-                .filter(x -> x.getClose() != null)
-                .sorted(Comparator.comparing(YahooFinanceQuote::getDate))
-                .collect(Collectors.toList()));
+        return vixCache.get(Pair.of(startDate, endDate), (pair) -> {
+                    // Break into 3 month chunks since yahoo finance is weird about long dates
+                    List<YahooFinanceQuote> quotes = new ArrayList<>();
+                    LocalDate sd = startDate;
+                    LocalDate ed = sd.plusMonths(3);
+                    while (sd.isBefore(endDate)) {
+                        quotes.addAll(historicStockDataLoadService.loadQuoteForDates("^VIX", sd, ed).stream()
+                                .filter(x -> x.getClose() != null)
+                                .collect(Collectors.toList()));
+                        sd = ed.plusDays(1);
+                        ed = sd.plusMonths(3);
+                    }
+                    return quotes.stream().filter(x -> (x.getDate().isAfter(startDate) || x.getDate().equals(startDate))
+                                    && (x.getDate().isBefore(endDate) || x.getDate().equals(endDate)))
+                            .sorted(Comparator.comparing(YahooFinanceQuote::getDate))
+                            .collect(Collectors.toList());
+                }
+        );
     }
 }
