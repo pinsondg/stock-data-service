ca4d61a23f545c7f4bc27b07b9d0751e1092232b
pinsondg <34725105+pinsondg@users.noreply.github.com>
2022-07-27 16:16:17 -0700

    Small refactor (#231)

--- src/main/java/com/dpgrandslam/stockdataservice/domain/service/VIXLoadService.java
+++ src/main/java/com/dpgrandslam/stockdataservice/domain/service/VIXLoadService.java
@@ -28,23 +28,7 @@ public class VIXLoadService {
 
     public List<YahooFinanceQuote> loadVIXBetweenDates(LocalDate startDate, LocalDate endDate) {
         log.info("Loading VIX data for dates {} - {}", startDate, endDate);
-        return vixCache.get(Pair.of(startDate, endDate), (pair) -> {
-                    // Break into 3 month chunks since yahoo finance is weird about long dates
-                    List<YahooFinanceQuote> quotes = new ArrayList<>();
-                    LocalDate sd = startDate;
-                    LocalDate ed = sd.plusMonths(3);
-                    while (sd.isBefore(endDate)) {
-                        quotes.addAll(historicStockDataLoadService.loadQuoteForDates("^VIX", sd, ed).stream()
-                                .filter(x -> x.getClose() != null)
-                                .collect(Collectors.toList()));
-                        sd = ed.plusDays(1);
-                        ed = sd.plusMonths(3);
-                    }
-                    return quotes.stream().filter(x -> (x.getDate().isAfter(startDate) || x.getDate().equals(startDate))
-                                    && (x.getDate().isBefore(endDate) || x.getDate().equals(endDate)))
-                            .sorted(Comparator.comparing(YahooFinanceQuote::getDate))
-                            .collect(Collectors.toList());
-                }
-        );
+        return vixCache.get(Pair.of(startDate, endDate), (pair) -> historicStockDataLoadService
+                .loadQuoteForDates("^VIX", startDate, endDate));
     }
 }
